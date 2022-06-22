f3cb4c3cd56da07545883b92def4dc6219252885
Serge Aleynikov <saleyn@gmail.com>
2022-06-22 14:35:21 -0400

    Add module

---
+++ src/fix_behavior.erl
@@ -0,0 +1,13 @@
+%%------------------------------------------------------------------------------
+%% @doc NIF FIX encoding/decoding functions
+%%------------------------------------------------------------------------------
+%% @copyright 2022 Serge Aleynikov
+%% The work is derived from Maxim Lapshin's open source work:
+%% https://github.com/maxlapshin/fix under the same open source MIT
+%% licensing terms as the original.
+%%------------------------------------------------------------------------------
+-module(fix_behavior).
+
+%% @doc This callback implements a consumer of FIX messages.
+%% The API is free in implement the FIX message processing using this behavior.
+-callback handle_fix(MsgType::atom(), MsgHeader::map(), Msg::map()) -> none().
