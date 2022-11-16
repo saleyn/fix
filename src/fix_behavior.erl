4bdd17f6033e745f47b3b8a0ecaca4bf738cf955
Serge Aleynikov <saleyn@gmail.com>
2022-11-16 09:40:16 -0500

    Update handle_fix callback spec

--- src/fix_behavior.erl
+++ src/fix_behavior.erl
@@ -8,9 +8,10 @@
 %% @doc This callback implements a consumer of FIX messages.
 %% Arguments are:
 %% <dl>
+%% <dt>Exchange</dt><dd>Exchange name</dd>
 %% <dt>MsgTp</dt><dd>Message Type (e.g. 'NewOrderSingle')</dd>
 %% <dt>Hdr</dt><dd>FIX header</dd>
 %% <dt>Msg</dt><dd>FIX message</dd>
 %% <dt>TS</dt><dd>Timestamp in microseconds since epoch</dd>
 %% </dl>
--callback handle_fix(MsgTp::atom(), Hdr::map(), Msg::map(), TS::integer()) -> any().
+-callback handle_fix(Xchg::atom(), MsgTp::atom(), Hdr::map(), Msg::map(), TS::integer()) -> any().
